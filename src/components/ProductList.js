94b3132c02a533c9f1b1061459ba31689ac75e41
BBrandford11 <brandfordbryce@gmail.com>
2022-05-18 15:14:50 -0600

    ads shippment component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,6 +1,10 @@
+
 import "./Productlist.css";
+import React, { useState } from "react";
 
 function ProductList(props) {
+
+  const [shipping, setShipping] = useState([])
   const deleteItems = (id) => {
     const attributes = { id };
     fetch("http://localhost:3001/", {
@@ -17,7 +21,31 @@ function ProductList(props) {
         console.log(err);
       });
   };
-  console.log(props.data);
+
+  const shipItems = (e,product) => {
+    e.preventDefault();
+    let { name, description, location, quantity, id } = product
+    quantity = quantity -1
+    const attributes = { name, description, location, quantity, id };
+    fetch("http://localhost:3001/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(attributes),
+    })
+      .then(() => {
+        console.log(`New Product added`);
+        shipmentItemList.push([product])
+
+        window.location.reload();
+      })
+
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const shipmentItemList = []
+
   return (
     <div className="Productlist">
       <div className="itemRows">
@@ -39,10 +67,23 @@ function ProductList(props) {
               {" "}
               Delete item
             </button>
-            <button>Edit item</button>
+            <button onClick={(e) => shipItems(e,product)} >Add to Shipments</button>
           </div>
         );
       })}
+      <h1>Shipments</h1>
+      {/* {shipmentItemList.map((product) => {
+        return (
+          <div key={product.product_id} className="product">
+          <p>{product.product_id}</p>
+          <p>{product.name}</p>
+          <p>{product.description}</p>
+          <p>{product.location}</p>
+          <p>{product.quantity}</p>
+      
+        </div>
+        );
+      })} */}
     </div>
   );
 }
